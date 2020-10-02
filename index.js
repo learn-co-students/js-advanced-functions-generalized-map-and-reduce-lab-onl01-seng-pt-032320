e10e2ffa51e871af4e9db519e7d6c25e0de14613
HMoreira2020 <moreiraheather@gmail.com>
2020-10-02 11:29:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your functions here
+
+
+let map = function(sourceArray, fn) {
+    return sourceArray.map(fn)
+}
+
+let reduce = function(sourceArray, fn, startingPoint = 0) {
+    return sourceArray.reduce(fn, startingPoint)
+}
+
+
+
+
+let sourceArray = [1,2,3,-9,""]
+
+
+// function reduceToTotal(sourceArray, startingPoint = 0) {
+//     sourceArray.forEach(element => startingPoint += element)
+//     return startingPoint
+//  }
+
+// function reduceToAllTrue(sourceArray) {
+//     let result = sourceArray.every( element  => Boolean(element) == true );
+//     return result
+// }
+
+
+// function reduceToAnyTrue(sourceArray) {
+//     let result = sourceArray.some( element  => Boolean(element) == true );
+//     return result
+// }
