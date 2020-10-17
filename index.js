3db954b39990d196a898d7f41e24789ff34fd809
nabilhayet <nabil4457@yahoo.com>
2020-10-17 00:00:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+
+let map = function(arr,callback){
+    let r =[]
+    for (let i = 0; i < arr.length; i++ ){
+         r.push(callback(arr[i]))
+    }
+    return r 
+}
+
+let reduce = function(arr,callback){
+    let s = 0
+    for (let i = 0; i < arr.length; i++ ){
+        s = s + (callback(arr[i],0))
+   }
+}
+
