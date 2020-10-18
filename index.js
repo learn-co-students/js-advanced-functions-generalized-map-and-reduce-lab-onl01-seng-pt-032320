ab542c0969ac0ca40ed82860cf3c7264acf99fe4
omarnabi1 <onabi20@gmail.com>
2020-10-18 19:47:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+const map = (array, callback) => {
+    return array.map(x => callback(x))
+    
+}
+
+
+const reduce = (array,callback,x) => {
+    let r = !!x ? x : array[0]
+    let i = !!x ? 0 : 1
+    
+    for (; i < array.length; i++){
+       r = callback(array[i], r)
+    }
+
+    return r
+
+}
