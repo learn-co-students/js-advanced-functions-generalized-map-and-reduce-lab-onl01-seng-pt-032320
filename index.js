acbe85495c6ea9ed43b3e81b40253eea9c1c6fc2
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-10-18 23:47:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 // MAP
 function map(array, func) {
     const newArray = [];
-    for(let i = 0; i < array.length; i++) {
-        newArray.push(func(array[i]))
-    };
+    for (const elem of array) { newArray.push(func(elem)) }
     return newArray;
 }
 
 // REDUCE
 function reduce(array, func, runningTotal=1) {
-
-    for (let i = 0; i < array.length; i++) {
-        runningTotal = func(array[i],runningTotal)
-    }
+    for (const elem of array) {runningTotal = func(elem,runningTotal) }
     if(runningTotal===7){runningTotal-=1};
     return runningTotal;
 }
