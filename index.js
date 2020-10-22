a6f51cb1695b617736172a1c996e06baa1cc19bd
pledgedavis <pledgedavis14@gmail.com>
2020-10-21 23:21:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map( array, callback) {
+    let r = []
+
+    for (let i = 0; i < array.length; i++) {
+        let theElement = array[i]
+        r.push(callback(theElement))
+    }
+    return r;
+}
+
+function reduce(array, callback, startingValue) {
+    let r = (!!startingValue) ? startingValue : array[0]
+    let i = (!!startingValue) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        r = callback(array[i], r)
+    }
+    return r;
+} 
